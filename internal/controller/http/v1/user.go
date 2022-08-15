949bff598fe33d4599653a292727d17d18af2608
DamirBashirov <dam.bashirov@gmail.com>
2022-08-15 23:48:50 +0300

    dev 01

--- internal/controller/http/v1/user.go
+++ internal/controller/http/v1/user.go
@@ -1,1 +1,22 @@
 package v1
+
+import (
+	"context"
+	"wimm/internal/domain/user"
+)
+
+const (
+	users = "/users"
+)
+
+type UserUsecase interface {
+	ListAllUsers(ctx context.Context) ([]user.User, error)
+}
+
+type userHandler struct {
+	userUsecase UserUsecase
+}
+
+func NewUserHander(userUsecase UserUsecase) *userHandler {
+	return &userHandler{userUsecase: userUsecase}
+}
