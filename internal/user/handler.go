458a600a4b814cb041e3f7cf79008069f4d727d4
DamirBashirov <dam.bashirov@gmail.com>
2022-08-21 18:19:28 +0300

    added find by email for users

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -3,17 +3,21 @@ package user
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 	"wimm/internal/handlers"
 	"wimm/internal/middleware"
+
 	// "wimm/internal/store"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 const (
-	usersURL = "/users"
-	userURL  = "/user/:uuid"
+	usersURL     = "/users"
+	userURL      = "/users/:id"
+	userEmailURL = "/useremail/:email"
 )
 
 type handler struct {
@@ -28,6 +32,8 @@ func NewHandler(repository Repository) handlers.Handler {
 
 func (h *handler) Register(router *httprouter.Router) {
 	router.HandlerFunc(http.MethodGet, usersURL, middleware.Middleware(h.GetList))
+	router.HandlerFunc(http.MethodGet, userURL, middleware.Middleware(h.Find))
+	router.HandlerFunc(http.MethodGet, userEmailURL, middleware.Middleware(h.FindByEmail))
 }
 
 func (h *handler) GetList(w http.ResponseWriter, r *http.Request) error {
@@ -38,6 +44,59 @@ func (h *handler) GetList(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	allBytes, err := json.Marshal(users)
+	if err != nil {
+		return err
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(allBytes)
+
+	return nil
+}
+
+func (h *handler) Find(w http.ResponseWriter, r *http.Request) error {
+
+	params := r.Context().Value(httprouter.ParamsKey).(httprouter.Params)
+	idStr := params.ByName("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		w.WriteHeader(400)
+		return err
+	}
+
+	u, err := h.repository.Find(context.TODO(), id)
+	if err != nil {
+		w.WriteHeader(400)
+		return err
+	}
+
+	allBytes, err := json.Marshal(u)
+	if err != nil {
+		return err
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(allBytes)
+
+	return nil
+}
+
+func (h *handler) FindByEmail(w http.ResponseWriter, r *http.Request) error {
+
+	params := r.Context().Value(httprouter.ParamsKey).(httprouter.Params)
+	email := params.ByName("email")
+	if email == "" {
+		w.WriteHeader(400)
+		return fmt.Errorf("undefined parameter <email>")
+	}
+
+	u, err := h.repository.FindByEmail(context.TODO(), email)
+	if err != nil {
+		w.WriteHeader(400)
+		return err
+	}
+
+	allBytes, err := json.Marshal(u)
 	if err != nil {
 		return err
 	}
